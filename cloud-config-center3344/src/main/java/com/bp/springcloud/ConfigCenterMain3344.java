3e598db61918289ed3f080594e4041af5506a817
daiyu <jiangyufeng.0918@gmail.com>
2020-04-17 17:05:15 +0800

    springcloud gateway client

---
+++ cloud-config-center3344/src/main/java/com/bp/springcloud/ConfigCenterMain3344.java
@@ -0,0 +1,20 @@
+package com.bp.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * @Auther: daiyu
+ * @Date: 17/4/20 15:39
+ * @Description:
+ */
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigCenterMain3344 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigCenterMain3344.class, args);
+    }
+}
+
