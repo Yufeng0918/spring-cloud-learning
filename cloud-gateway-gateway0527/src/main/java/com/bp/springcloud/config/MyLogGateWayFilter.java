c07d4e4fc1c4413d67330e32d984001b0ccfd231
daiyu <jiangyufeng.0918@gmail.com>
2020-04-17 15:04:24 +0800

    springcloud gateway

---
+++ cloud-gateway-gateway0527/src/main/java/com/bp/springcloud/config/MyLogGateWayFilter.java
@@ -0,0 +1,41 @@
+package com.bp.springcloud.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.Ordered;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+import java.time.LocalDate;
+
+/**
+ * @Auther: daiyu
+ * @Date: 17/4/20 14:54
+ * @Description:
+ */
+@Component
+@Slf4j
+public class MyLogGateWayFilter implements GlobalFilter, Ordered {
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        log.info("Global Filter: " + LocalDate.now());
+
+        String username = exchange.getRequest().getQueryParams().getFirst("username");
+        if (StringUtils.isEmpty(username)) {
+            log.info("unauthorized access");
+            exchange.getResponse().setStatusCode(HttpStatus.NOT_ACCEPTABLE);
+            return exchange.getResponse().setComplete();
+        }
+
+        return chain.filter(exchange);
+    }
+
+    @Override
+    public int getOrder() {
+        return 0;
+    }
+}
