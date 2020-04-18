a97302dc8b77dcb2f45799f2d4c8c113aea4239f
daiyu <jiangyufeng.0918@gmail.com>
2020-04-18 17:34:14 +0800

    spring cloud nacos-config

---
+++ cloudalibaba-consumer-nacos-order83/src/main/java/com/bp/springcloud/alibaba/config/ApplicationContextConfig.java
@@ -0,0 +1,20 @@
+package com.bp.springcloud.alibaba.config;
+
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * @auther zzyy
+ * @create 2020-02-23 14:45
+ */
+@Configuration
+public class ApplicationContextConfig {
+    @Bean
+    @LoadBalanced
+    public RestTemplate getRestTemplate() {
+
+        return new RestTemplate();
+    }
+}
