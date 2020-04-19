8858a8147940a22fc30bea230d33decb8b7b0b12
daiyu <jiangyufeng.0918@gmail.com>
2020-04-19 18:44:27 +0800

    spring cloud sentinel limit

---
+++ cloudalibaba-consumer-nacos-order84/src/main/java/com/bp/springcloud/alibaba/service/PaymentService.java
@@ -0,0 +1,18 @@
+package com.bp.springcloud.alibaba.service;
+
+import com.bp.springcloud.entities.CommonResult;
+import com.bp.springcloud.entities.Payment;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+/**
+ * @auther zzyy
+ * @create 2020-02-25 18:15
+ */
+@FeignClient(value = "nacos-payment-provider",fallback = PaymentFallbackService.class)
+public interface PaymentService
+{
+    @GetMapping(value = "/paymentSQL/{id}")
+    public CommonResult<Payment> paymentSQL(@PathVariable("id") Long id);
+}
--- cloudalibaba-sentinel-service8401/src/main/java/com/bp/springcloud/alibaba/myhandler/CustomerBlockHandler.java
+++ cloudalibaba-sentinel-service8401/src/main/java/com/bp/springcloud/alibaba/myhandler/CustomerBlockHandler.java
@@ -3,17 +3,14 @@ package com.bp.springcloud.alibaba.myhandler;
 import com.alibaba.csp.sentinel.slots.block.BlockException;
 import com.bp.springcloud.entities.CommonResult;
 
-/**
- * @auther zzyy
- * @create 2020-02-25 15:32
- */
+
 public class CustomerBlockHandler {
-//    public static CommonResult handlerException(BlockException exception)
-//    {
-//        return new CommonResult(4444,"按客戶自定义,global handlerException----1");
-//    }
-//    public static CommonResult handlerException2(BlockException exception)
-//    {
-//        return new CommonResult(4444,"按客戶自定义,global handlerException----2");
-//    }
+
+    public static CommonResult handlerException(BlockException exception) {
+        return new CommonResult(445,"customized global handlerException----1");
+    }
+
+    public static CommonResult handlerException2(BlockException exception) {
+        return new CommonResult(444,"customized global handlerException----2");
+    }
 }
