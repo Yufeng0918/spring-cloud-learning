0f670bd7a9ae3ae58ce43dd0c566d068019ec20f
daiyu <jiangyufeng.0918@gmail.com>
2020-04-19 17:45:22 +0800

    spring cloud sentinel limit

--- cloudalibaba-sentinel-service8401/src/main/java/com/bp/springcloud/alibaba/controller/RateLimitController.java
+++ cloudalibaba-sentinel-service8401/src/main/java/com/bp/springcloud/alibaba/controller/RateLimitController.java
@@ -15,31 +15,28 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class RateLimitController {
 
-//    @GetMapping("/byResource")
-//    @SentinelResource(value = "byResource",blockHandler = "handleException")
-//    public CommonResult byResource()
-//    {
-//        return new CommonResult(200,"按资源名称限流测试OK",new Payment(2020L,"serial001"));
-//    }
-//    public CommonResult handleException(BlockException exception)
-//    {
-//        return new CommonResult(444,exception.getClass().getCanonicalName()+"\t 服务不可用");
-//    }
-//
-//    @GetMapping("/rateLimit/byUrl")
-//    @SentinelResource(value = "byUrl")
-//    public CommonResult byUrl()
-//    {
-//        return new CommonResult(200,"按url限流测试OK",new Payment(2020L,"serial002"));
-//    }
-//
-//
-//    @GetMapping("/rateLimit/customerBlockHandler")
-//    @SentinelResource(value = "customerBlockHandler",
-//            blockHandlerClass = CustomerBlockHandler.class,
-//            blockHandler = "handlerException2")
-//    public CommonResult customerBlockHandler()
-//    {
-//        return new CommonResult(200,"按客戶自定义",new Payment(2020L,"serial003"));
-//    }
+    @GetMapping("/byResource")
+    @SentinelResource(value = "byResource",blockHandler = "handleException")
+    public CommonResult byResource() {
+        return new CommonResult(200,"limit by resource name OK",new Payment(2020L,"serial001"));
+    }
+
+    public CommonResult handleException(BlockException exception) {
+        return new CommonResult(444,exception.getClass().getCanonicalName()+"\t service unavaialbe");
+    }
+
+    @GetMapping("/rateLimit/byUrl")
+    @SentinelResource(value = "byUrl")
+    public CommonResult byUrl() {
+        return new CommonResult(200,"limit by url OK",new Payment(2020L,"serial002"));
+    }
+
+
+    @GetMapping("/rateLimit/customerBlockHandler")
+    @SentinelResource(value = "customerBlockHandler",
+            blockHandlerClass = CustomerBlockHandler.class,
+            blockHandler = "handlerException2")
+    public CommonResult customerBlockHandler() {
+        return new CommonResult(200,"customized",new Payment(2020L,"serial003"));
+    }
 }
