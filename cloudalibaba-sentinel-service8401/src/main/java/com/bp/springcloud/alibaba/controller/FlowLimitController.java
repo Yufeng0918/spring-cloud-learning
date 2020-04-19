16e58ca8933a2caae884434dbb75a3de8b816328
daiyu <jiangyufeng.0918@gmail.com>
2020-04-19 15:31:33 +0800

    spring cloud sentinel for hotkey

--- cloudalibaba-sentinel-service8401/src/main/java/com/bp/springcloud/alibaba/controller/FlowLimitController.java
+++ cloudalibaba-sentinel-service8401/src/main/java/com/bp/springcloud/alibaba/controller/FlowLimitController.java
@@ -9,10 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.concurrent.TimeUnit;
 
-/**
- * @auther zzyy
- * @create 2020-02-24 16:26
- */
 @RestController
 @Slf4j
 public class FlowLimitController  {
@@ -46,16 +42,17 @@ public class FlowLimitController  {
         int age = 10/0;
         return "------testE Exception Number";
     }
-//
-//    @GetMapping("/testHotKey")
-//    @SentinelResource(value = "testHotKey",blockHandler = "deal_testHotKey")
-//    public String testHotKey(@RequestParam(value = "p1",required = false) String p1,
-//                             @RequestParam(value = "p2",required = false) String p2) {
-//        //int age = 10/0;
-//        return "------testHotKey";
-//    }
-//    public String deal_testHotKey (String p1, String p2, BlockException exception) {
-//        return "------deal_testHotKey,o(╥﹏╥)o";  //sentinel系统默认的提示：Blocked by Sentinel (flow limiting)
-//    }
+
+    @GetMapping("/testHotKey")
+    @SentinelResource(value = "testHotKey",blockHandler = "deal_testHotKey")
+    public String testHotKey(@RequestParam(value = "p1",required = false) String p1,
+                             @RequestParam(value = "p2",required = false) String p2) {
+        //int age = 10/0;
+        return "------testHotKey";
+    }
+
+    public String deal_testHotKey (String p1, String p2, BlockException exception) {
+        return "fail to testHotKey";
+    }
 
 }
